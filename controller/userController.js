5266d8974f0263b57eeb526bc4c5230b93af7063
USer99pro <akbbna48@gmail.com>
2025-06-13 16:51:41 +0700

    update

---
+++ controller/userController.js
@@ -0,0 +1,33 @@
+
+exports.showAlluser = (res, req, next) => {
+    res.status(200).json({
+        name: "armmy",
+        age: 21 
+
+    })
+}
+
+exports.user1 = (req, res, netxt) => {
+    res.status(200).json({
+         name: "mum",
+        age: 21 ,
+    })
+}
+exports.user1 = (req, res, netxt) => {
+    res.status(200).json({
+         name: "mum",
+        age: 21 ,
+    })
+}
+exports.user2 = (req, res, netxt) => {
+    res.status(200).json({
+         name: "paint",
+        age: 21 ,
+    })
+}
+exports.user3 = (req, res, netxt) => {
+    res.status(200).json({
+         name: "tem",
+        age: 21 ,
+    })
+}
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const usercontroller = require("../controller/userController");
+
+/* GET users listing. */
+router.get('/', usercontroller.showAlluser);
+router.get('/1', usercontroller.user1);
+router.get('/2', usercontroller.user2);
+router.get('/3', usercontroller.user3);
+
+
+
+module.exports = router;
