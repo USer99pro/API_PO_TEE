983462cba981078cbff19d77567acbbf9e5fea2b
USer99pro <akbbna48@gmail.com>
2025-07-22 08:06:40 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-// โหลด .env
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const studentRouter = require('./routes/studentRouter');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const studentRouter = require("./routes/studentRouter");
 
 const app = express();
 
 // เชื่อมต่อ MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('✅ Connected to MongoDB');
-}).catch((err) => {
-    console.error('❌ Failed to connect MongoDB:', err.message);
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ Failed to connect MongoDB:", err.message);
+  });
+
+
+  mongoose.connection('<<Mongodb URL>>/<< Database Name >>').then(() => {
+    console.log("✅ Connected to MongoDB");
+  }).catch((err) => {
+    console.error("❌ Failed to connect MongoDB:", err.message);
+  }
 
 // Middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/students', studentRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/students", studentRouter);
 
 module.exports = app;
